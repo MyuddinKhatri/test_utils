3dbc3700b77d3d8a7a542e2d85d25be1bb73093d
Myuddin khatri <khatrimayu111@gmail.com>
2024-05-21 10:26:31 +0530

    pre-commit: pre-commit hooks

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 
 from test_utils.utils.conftest import monkeymodule, db_instance
 from test_utils.utils.customize import load_customizations, add_customization_hash
-
+from test_utils.pre_commit.validate_copyright import validate_copyright
 
 __version__ = "0.2.2"
