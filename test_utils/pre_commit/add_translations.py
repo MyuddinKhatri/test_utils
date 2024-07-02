49da3c368f22ce4771afbd81d98c24a6c0645fd3
Myuddin khatri <khatrimayu111@gmail.com>
2024-07-02 11:00:33 +0530

    feat: add translations

--- test_utils/pre_commit/add_translations.py
+++ test_utils/pre_commit/add_translations.py
@@ -4,39 +4,28 @@ import argparse
 import pathlib
 from typing import Sequence
 
+try:
+	from frappe.translate import get_untranslated, update_translations
+except Exception as e:
+	raise(e)
+
 logging.basicConfig(filename='hook.log', level=logging.INFO, format='%(message)s')
 logger = logging.getLogger(__name__)
 
-def setup_frappe():
-	bench_path = pathlib.Path().resolve().parent.parent
-	os.environ['FRAPPE_SITE_NAME'] = 'localhost.demand'
-	os.environ['FRAPPE_BENCH_PATH'] = str(bench_path)
-	import frappe
-	frappe.init(site=os.environ['FRAPPE_SITE_NAME'])
-	frappe.connect()
-	return frappe
+def add_translations(lang, app):
+	untranslated_file = "untranslated_strings"
+	translated_file = "translated_strings"
+	get_untranslated(lang=lang, untranslated_file=untranslated_file, app=app)
+	update_translations(lang=lang, untranslated_file=untranslated_file, translated_file=translated_file, app=app)
 
-def add_translations(frappe, lang, app):
-	logger.info(f"In the fn")
-	from frappe.translate import get_untranslated
-	get_untranslated(lang, f"../apps/{app}/random.txt", app)
 
 def main(argv: Sequence[str] = None):
-	logger.info(f"In the main")
 	parser = argparse.ArgumentParser()
 	parser.add_argument('filenames', nargs='*')
-	parser.add_argument('--app', action='append', help='App to execute pre-commit hooks on')
-	parser.add_argument('--lang', action='append', help='Language for translation')
+	parser.add_argument('--lang', action='append', help='Language to translate strings')
+	parser.add_argument('--app', action='append', help='App to get untranslated string and translate them')
 	args = parser.parse_args(argv)
 
-	app = "beam"  # Default app
-	lang = "en"   # Default language
-	if app:
-		frappe = setup_frappe()
-		try:
-			add_translations(frappe, lang, app)
-		finally:
-			frappe.destroy()
-
-if __name__ == '__main__':
-	main()
+	lang = args.lang[0]
+	app = args.app[0]
+	add_translations(lang, app)
